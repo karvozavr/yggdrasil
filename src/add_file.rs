b84b28356f0ff798f18d597b697b994fec3f289b
Dmitrii Abramov <karvozavr@gmail.com>
2021-12-07 15:21:46 +0000

    Lint code

--- src/add_file.rs
+++ src/add_file.rs
@@ -1,7 +1,4 @@
-use std::{
-    fmt::{format, Error},
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
 use uuid::Uuid;
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use yggdrasil::add_file::{add_document, YggId};
-
 fn main() {
     println!("Hello, world!");
 }
